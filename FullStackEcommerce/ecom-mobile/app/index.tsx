f24e159dbf200ae50fa38bc49b353b84a12a69d5
Pournima Bhosale <pournimabhosale2412@gmail.com>
2024-11-21 16:31:41 -0500

    Product display

--- FullStackEcommerce/ecom-mobile/app/index.tsx
+++ FullStackEcommerce/ecom-mobile/app/index.tsx
@@ -4,15 +4,10 @@ import ProductListItem from "../components/ProductListItem";
 import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen(){
-    return(
-        <Button variant="outline">
-            <ButtonText>Press me</ButtonText>
-        </Button>
-    )    
-    /*return (
+    return (
         <FlatList 
         data={products}
         renderItem={({item})=><ProductListItem product={item}/>}
         />
-    );*/
+    );
 }
